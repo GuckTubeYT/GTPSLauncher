38dd9549bd50d1e0568b60cae633c111113c3ee6
GuckTubeYT <56192597+GuckTubeYT@users.noreply.github.com>
2019-12-12 08:36:54 +0700

    Add files via upload

---
+++ GuckProject/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Turn ON"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(165, 37)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 25)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Turn OFF"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'lblMsg
+        '
+        Me.lblMsg.Location = New System.Drawing.Point(12, 9)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(283, 25)
+        Me.lblMsg.TabIndex = 2
+        Me.lblMsg.Text = "GTPS Is Turn OFF"
+        Me.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(15, 68)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(124, 42)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Server ON/OFF Checker"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(166, 70)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(128, 40)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Changelog"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 122)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "GTPS Launcher"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents lblMsg As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ GuckProject/Form1.vb
@@ -0,0 +1,148 @@
+﻿'This Application Made By: GuckTube YT
+'Owner GTIT Private Server: iTavage
+'Dont forget to Subscribe: GuckTube YT
+'Also Dont forget to subscribe my friends
+'Kocak Z
+'Fakemodz
+'Jacky Harmonis
+'Poor GT
+'My Discord: GuckTubeYT#8912
+Imports System.Net
+Imports System.IO
+
+
+Public Class Form1
+    'Turn ON
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Delete File
+        My.Computer.FileSystem.DeleteFile("C:\Windows\System32\drivers\etc\hosts")
+        'Update Message
+        lblMsg.Text = "GTPS Is Turn ON"
+        'Edit File
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("C:\Windows\System32\drivers\etc\hosts", True)
+        file.WriteLine("# Copyright (c) 1993-2009 Microsoft Corp. ")
+        file.WriteLine("# ")
+        file.WriteLine("# This is a sample HOSTS file used by Microsoft TCP/IP for Windows. ")
+        file.WriteLine("# ")
+        file.WriteLine("# This file contains the mappings of IP addresses to host names. Each ")
+        file.WriteLine("# entry should be kept on an individual line. The IP address should ")
+        file.WriteLine("# be placed in the first column followed by the corresponding host name. ")
+        file.WriteLine("# The IP address and the host name should be separated by at least one ")
+        file.WriteLine("# space. ")
+        file.WriteLine("# ")
+        file.WriteLine("# Additionally, comments (such as these) may be inserted on individual ")
+        file.WriteLine("# lines or following the machine name denoted by a '#' symbol. ")
+        file.WriteLine("# ")
+        file.WriteLine("# For example: ")
+        file.WriteLine("# ")
+        file.WriteLine("#      102.54.94.97     rhino.acme.com          # source server ")
+        file.WriteLine("#       38.25.63.10     x.acme.com              # x client host ")
+        file.WriteLine(" ")
+        file.WriteLine("# localhost name resolution is handled within DNS itself. ")
+        file.WriteLine("#      127.0.0.1       localhost ")
+        file.WriteLine("#      ::1             localhost ")
+        'You Can Change IP VPS
+        file.WriteLine("YourIPVPS growtopia1.com")
+        file.WriteLine("YourIPVPS growtopia2.com")
+        file.Close()
+
+    End Sub
+    'Turn OFF
+    'No need to change this
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'Delete File
+        My.Computer.FileSystem.DeleteFile("C:\Windows\System32\drivers\etc\hosts")
+        'Updated Message
+        lblMsg.Text = "GTPS Is Turn OFF"
+        'Edit File
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("C:\Windows\System32\drivers\etc\hosts", True)
+        file.WriteLine("# Copyright (c) 1993-2009 Microsoft Corp. ")
+        file.WriteLine("# ")
+        file.WriteLine("# This is a sample HOSTS file used by Microsoft TCP/IP for Windows. ")
+        file.WriteLine("# ")
+        file.WriteLine("# This file contains the mappings of IP addresses to host names. Each ")
+        file.WriteLine("# entry should be kept on an individual line. The IP address should ")
+        file.WriteLine("# be placed in the first column followed by the corresponding host name. ")
+        file.WriteLine("# The IP address and the host name should be separated by at least one ")
+        file.WriteLine("# space. ")
+        file.WriteLine("# ")
+        file.WriteLine("# Additionally, comments (such as these) may be inserted on individual ")
+        file.WriteLine("# lines or following the machine name denoted by a '#' symbol. ")
+        file.WriteLine("# ")
+        file.WriteLine("# For example: ")
+        file.WriteLine("# ")
+        file.WriteLine("#      102.54.94.97     rhino.acme.com          # source server ")
+        file.WriteLine("#       38.25.63.10     x.acme.com              # x client host ")
+        file.WriteLine(" ")
+        file.WriteLine("# localhost name resolution is handled within DNS itself. ")
+        file.WriteLine("#      127.0.0.1       localhost ")
+        file.WriteLine("#      ::1             localhost ")
+        file.Close()
+    End Sub
+    'Check Status Server ON/OFF
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Try
+            Using client = New System.Net.WebClient()
+                'You can Change Google.com to anything
+                'Example: www.bing.com
+                Using Stream = client.OpenRead("http://www.google.com")
+                    'No Need to change this
+                    'If error, you can change this
+                    Dim TempPath As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "C:\Server\Status.txt"
+                    If File.Exists("C:\Server\Status.txt") Then
+                        My.Computer.FileSystem.DeleteFile("C:\Server\Status.txt")
+                    End If
+                    'You Can Change this
+                    My.Computer.Network.DownloadFile("http://IPVPS/PATH/NAME.TXT", "C:\Server\Status.txt")
+                    'No need to change this
+                    'if error, you can change this
+                    Dim Reader As New IO.StreamReader("C:\Server\Status.txt")
+                    Dim FileContents As String = Reader.ReadToEnd
+                    Reader.Close()
+
+                    MessageBox.Show(FileContents) 'Not really neccesary, but just to see that we have the string
+
+                    'Then do whatever you need to do with your string
+                End Using
+            End Using
+        Catch
+            MessageBox.Show("Your Internet Is Offline")
+        End Try
+    End Sub
+    'Changelog, IDK This is will Useful or no
+    'You can Delete Changelog by Delete Changelog Button
+    'if you think it's useful, don't delete Changelog button
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Try
+            Using client = New System.Net.WebClient()
+                'You can Change Google.com to anything
+                'Example: www.bing.com
+                Using Stream = client.OpenRead("http://www.google.com")
+                    'No Need to change this
+                    'If error, you can change this
+                    Dim TempPath As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "C:\Server\Changelog.txt"
+                    If File.Exists("C:\Server\Changelog.txt") Then
+                        My.Computer.FileSystem.DeleteFile("C:\Server\Changelog.txt")
+                    End If
+                    'You Can Change this
+                    My.Computer.Network.DownloadFile("http://IPVPS/PATH/NAME.TXT", "C:\Server\Changelog.txt")
+                    'No Need to change this
+                    'If error, you can change this
+                    Dim Reader As New IO.StreamReader("C:\Server\Changelog.txt")
+                    Dim FileContents As String = Reader.ReadToEnd
+                    Reader.Close()
+
+                    MessageBox.Show(FileContents) 'Not really neccesary, but just to see that we have the string
+
+                    'Then do whatever you need to do with your string
+                End Using
+            End Using
+        Catch
+            MessageBox.Show("Your Internet Is Offline")
+        End Try
+    End Sub
+End Class
+
+
